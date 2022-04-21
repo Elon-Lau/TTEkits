3e908a494215f1d0f4b0a3f0bb02dc81b7e287c6
Elon-Lau <weitinglau1999@gmail.com>
2022-04-21 11:45:29 +0800

    update

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-from setuptools import setup, find_packages
-import codecs
-import os
+from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-VERSION = '0.0.1'
-DESCRIPTION = 'A basic hello package'
-
-# Setting up
 setup(
-    name="ttekits",
-    version=VERSION,
+    name="helloworld",
+    version='0.0.1',
+    description="Say hello!",
+    py_modules=["helloworld"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    extras_require = {"dev":[],},
+    url="https://github.com/Elon-Lau/TTEkits",
     author="Elon Lau",
     author_email="weitinglau1999@gmail.com",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    install_requires=['numpy'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    setup_requires=['wheel']
 )
